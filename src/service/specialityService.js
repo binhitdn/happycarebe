6e6ec4b3e561be8a3603ff62e6c45acc0cbdf8f1
Bình Phan <binhpc.21it@vku.udn.vn>
2022-12-13 17:01:25 +0700

    first commit

---
+++ src/service/specialityService.js
@@ -0,0 +1,69 @@
+import db from '../models/index';
+const { Op } = require("sequelize");
+
+let getAllSpecialities = async () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let specialities = await db.specialties.findAll({
+                where: {
+                    id: {
+                        [Op.gt]: 1
+                    }
+                },
+                attributes: ['name', 'image','id'],
+                
+            },
+            )
+           
+            resolve(specialities)
+
+        } catch (e) {
+            resolve(e)
+        }
+        
+    })
+}
+let createNewSpeciality = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let message = await db.specialties.create(
+                {
+                    name: data.name,
+                    contentHTML: data.contentHTML,
+                    contentMarkdown: data.contentMarkdown,
+                    image: data.avatar,
+                    description: data.description              
+                }
+            )
+            
+            resolve(message)
+        } catch (e) {
+            resolve(e)
+        }
+    }
+    )
+}
+let getSpecialityById = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let message = await db.specialties.findOne({
+                where: {
+                    id: data
+                },
+                raw: true
+            },
+           )    
+            resolve(message)
+        } catch (e) {
+            resolve(e)
+        }
+    }
+    )
+}
+
+module.exports = {
+    getAllSpecialities: getAllSpecialities,
+    createNewSpeciality: createNewSpeciality,
+    getSpecialityById: getSpecialityById
+   
+}
